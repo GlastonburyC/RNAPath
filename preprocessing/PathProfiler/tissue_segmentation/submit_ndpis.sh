714e754e63f9ef8054ac03f0bc20176e673700ba
francescocister <115783390+francescocister@users.noreply.github.com>
2023-08-23 17:39:43 +0200

    Add files via upload

---
+++ preprocessing/PathProfiler/tissue_segmentation/submit_ndpis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# SLURM options
+#SBATCH --time=16:00:00 #define runtime
+#SBATCH --mem=32G        #define amount of ram
+#SBATCH --partition=gpu 
+#SBATCH --mem=64G 
+#SBATCH --gpus-per-node=2
+
+python3 run.py --model ../../checkpoint_ts.pth --gpu_id 0,1 --save_folder /hpc/dhl_ec/VirtualSlides/cglastonbury/PathProfiler/tissue-segmentation/masks/ --slide_dir /hpc/dhl_ec/VirtualSlides/HE/_ndpi/_ndpis/ --mask_magnification 2.5
