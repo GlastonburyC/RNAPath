4575a3e097c79a54da1b2cda26fb2daf759c208a
francescocister <115783390+francescocister@users.noreply.github.com>
2023-08-24 22:59:37 +0200

    Add files via upload

---
+++ datasets/dataset_generic.py
@@ -0,0 +1,372 @@
+from __future__ import print_function, division
+import os
+import torch
+import numpy as np
+import pandas as pd
+import torch
+from scipy import stats
+import random
+from torch.utils.data import Dataset
+
+from utils.utils import generate_split, nth
+
+
+os.environ['OMP_DISPLAY_ENV'] = '0'
+
+def save_splits(split_datasets, column_keys, filename, boolean_style=False):
+	splits = [split_datasets[i].slide_data['slide_id'] for i in range(len(split_datasets))]
+	if not boolean_style:
+		df = pd.concat(splits, ignore_index=True, axis=1)
+		df.columns = column_keys
+	else:
+		df = pd.concat(splits, ignore_index = True, axis=0)
+		index = df.values.tolist()
+		one_hot = np.eye(len(split_datasets)).astype(bool)
+		bool_array = np.repeat(one_hot, [len(dset) for dset in split_datasets], axis=0)
+		df = pd.DataFrame(bool_array, index=index, columns = ['train', 'val', 'test'])
+
+	df.to_csv(filename)
+	print()
+
+class Generic_WSI_Classification_Dataset(Dataset):
+	def __init__(self,
+		csv_path = '',
+		shuffle = False, 
+		seed = 1, 
+		print_info = True,
+		label_dict = {},
+		filter_dict = {},
+		ignore=[],
+		rna_seq_csv = './RNA-SEQ-Analysis/rnaseq_complete.csv'
+		):
+		"""
+		Args:
+			csv_file (string): Path to the csv file with annotations.
+			shuffle (boolean): Whether to shuffle
+			seed (int): random seed for shuffling the data
+			print_info (boolean): Whether to print a summary of the dataset
+			label_dict (dict): Dictionary with key, value pairs for converting str labels to int
+			ignore (list): List containing class labels to ignore
+		"""
+		self.label_dict = label_dict
+		self.num_classes = len(set(self.label_dict.values()))
+		self.seed = seed
+		self.print_info = print_info
+		self.train_ids, self.val_ids, self.test_ids  = (None, None, None)
+		self.data_dir = None
+		self.rna_seq_csv = rna_seq_csv
+
+		slide_data = pd.read_csv(csv_path)
+		slide_data = self.filter_df(slide_data, filter_dict)
+		slide_data = self.df_prep(slide_data, self.label_dict, ignore)
+
+		###shuffle data
+		if shuffle:
+			np.random.seed(seed)
+			np.random.shuffle(slide_data)
+
+		self.slide_data = slide_data
+
+		self.patient_data_prep(patient_voting='max')
+		self.cls_ids_prep()
+
+
+	def cls_ids_prep(self):
+		# store ids corresponding each class at the patient or case level
+		self.patient_cls_ids = [[] for i in range(self.num_classes)]		
+		for i in range(self.num_classes):
+			self.patient_cls_ids[i] = np.where(self.patient_data['label'] == i)[0]
+
+		# store ids corresponding each class at the slide level
+		self.slide_cls_ids = [[] for i in range(self.num_classes)]
+		for i in range(self.num_classes):
+			self.slide_cls_ids[i] = np.where(self.slide_data['label'] == i)[0]
+
+	def patient_data_prep(self, patient_voting='max'):
+		patients = (np.array(self.slide_data['case_id'])) # get unique patients
+		patient_labels = []
+		
+		for p in patients:
+			locations = self.slide_data[self.slide_data['case_id'] == p].index.tolist()
+			assert len(locations) > 0
+			label = self.slide_data['label'][locations].values
+			if patient_voting == 'max':
+				label = label.max() # get patient label (MIL convention)
+			elif patient_voting == 'maj':
+				label = stats.mode(label)[0]
+			else:
+				raise NotImplementedError
+			patient_labels.append(label)
+		
+		self.patient_data = {'case_id':patients, 'label':np.array(patient_labels)}
+
+	@staticmethod
+	def df_prep(data, label_dict, ignore):
+		mask = data['label'].isin(ignore)
+		data = data[~mask]
+		data.reset_index(drop=True, inplace=True)
+		
+		# No need to move from string label to number as the label is 
+		# a vector of numbers here
+
+		for i in data.index:
+			key = data.loc[i, 'label']
+			data.at[i, 'label'] = label_dict[key]
+
+		return data
+
+	def filter_df(self, df, filter_dict={}):
+		if len(filter_dict) > 0:
+			filter_mask = np.full(len(df), True, bool)
+			# assert 'label' not in filter_dict.keys()
+			for key, val in filter_dict.items():
+				mask = df[key].isin(val)
+				filter_mask = np.logical_and(filter_mask, mask)
+			df = df[filter_mask]
+		return df
+
+	def __len__(self):
+		
+		return len(self.slide_data)
+
+	def create_splits(self, k = 3, val_num = (25, 25), test_num = (40, 40), label_frac = 1.0, custom_test_ids = None):
+		settings = {
+					'n_splits' : k, 
+					'val_num' : val_num, 
+					'test_num': test_num,
+					'label_frac': label_frac,
+					'seed': self.seed,
+					'custom_test_ids': custom_test_ids
+					}
+
+		settings.update({'cls_ids' : self.slide_cls_ids, 'samples': len(self.slide_data)})
+
+		self.split_gen = generate_split(**settings)
+
+	def set_splits(self,start_from=None):
+		if start_from:
+			ids = nth(self.split_gen, start_from)
+
+		else:
+			ids = next(self.split_gen)
+		
+		self.train_ids, self.val_ids, self.test_ids = ids
+
+	def get_split_from_df(self, all_splits, split_key='train'):
+		split = all_splits[split_key]
+		split = split.dropna().reset_index(drop=True)
+
+		if len(split) > 0:
+			mask = self.slide_data['slide_id'].isin(split.tolist())
+			df_slice = self.slide_data[mask].reset_index(drop=True)
+			split = Generic_Split(df_slice, data_dir=self.data_dir, num_classes=self.num_classes, rna_seq_csv=self.rna_seq_csv, genes=self.genes, bag_dropout=self.bag_dropout)
+		else:
+			split = None
+		
+		return split
+
+	def get_merged_split_from_df(self, all_splits, split_keys=['train']):
+		merged_split = []
+		for split_key in split_keys:
+			split = all_splits[split_key]
+			split = split.dropna().reset_index(drop=True).tolist()
+			merged_split.extend(split)
+
+		if len(split) > 0:
+			mask = self.slide_data['slide_id'].isin(merged_split)
+			df_slice = self.slide_data[mask].reset_index(drop=True)
+			split = Generic_Split(df_slice, data_dir=self.data_dir, num_classes=self.num_classes)
+		else:
+			split = None
+		
+		return split
+
+
+	def return_splits(self, from_id=True, csv_path=None):
+
+		# Either from_id or from an already existing csv file indicating the splits
+
+		if from_id:
+			if len(self.train_ids) > 0:
+				train_data = self.slide_data.loc[self.train_ids].reset_index(drop=True)
+				train_split = Generic_Split(train_data, data_dir=self.data_dir, num_classes=self.num_classes)
+
+			else:
+				train_split = None
+			
+			if len(self.val_ids) > 0:
+				val_data = self.slide_data.loc[self.val_ids].reset_index(drop=True)
+				val_split = Generic_Split(val_data, data_dir=self.data_dir, num_classes=self.num_classes)
+
+			else:
+				val_split = None
+			
+			if len(self.test_ids) > 0:
+				test_data = self.slide_data.loc[self.test_ids].reset_index(drop=True)
+				test_split = Generic_Split(test_data, data_dir=self.data_dir, num_classes=self.num_classes)
+			
+			else:
+				test_split = None
+			
+		
+		else:
+			assert csv_path 
+			all_splits = pd.read_csv(csv_path, dtype=self.slide_data['slide_id'].dtype)  # Without "dtype=self.slide_data['slide_id'].dtype", read_csv() will convert all-number columns to a numerical type. Even if we convert numerical columns back to objects later, we may lose zero-padding in the process; the columns must be correctly read in from the get-go. When we compare the individual train/val/test columns to self.slide_data['slide_id'] in the get_split_from_df() method, we cannot compare objects (strings) to numbers or even to incorrectly zero-padded objects/strings. An example of this breaking is shown in https://github.com/andrew-weisman/clam_analysis/tree/main/datatype_comparison_bug-2021-12-01.
+			train_split = self.get_split_from_df(all_splits, 'train')
+			val_split = self.get_split_from_df(all_splits, 'val')
+			test_split = self.get_split_from_df(all_splits, 'test')
+			
+		return train_split, val_split, test_split
+
+	def get_list(self, ids):
+		return self.slide_data['slide_id'][ids]
+
+	def getlabel(self, ids):
+		return self.slide_data['label'][ids]
+
+	def __getitem__(self, idx):
+		return None
+
+	def test_split_gen(self, return_descriptor=False):
+
+		if return_descriptor:
+			index = [list(self.label_dict.keys())[list(self.label_dict.values()).index(i)] for i in range(self.num_classes)]
+			columns = ['train', 'val', 'test']
+			df = pd.DataFrame(np.full((len(index), len(columns)), 0, dtype=np.int32), index= index,
+							columns= columns)
+		count = len(self.train_ids)
+		print('\nnumber of training samples: {}'.format(count))
+		labels = self.getlabel(self.train_ids)
+		unique, counts = np.unique(labels, return_counts=True)
+		for u in range(len(unique)):
+			print('number of samples in cls {}: {}'.format(unique[u], counts[u]))
+			if return_descriptor:
+				df.loc[index[u], 'train'] = counts[u]
+		
+		count = len(self.val_ids)
+		print('\nnumber of val samples: {}'.format(count))
+		labels = self.getlabel(self.val_ids)
+		unique, counts = np.unique(labels, return_counts=True)
+		for u in range(len(unique)):
+			print('number of samples in cls {}: {}'.format(unique[u], counts[u]))
+			if return_descriptor:
+				df.loc[index[u], 'val'] = counts[u]
+
+		count = len(self.test_ids)
+		print('\nnumber of test samples: {}'.format(count))
+		labels = self.getlabel(self.test_ids)
+		unique, counts = np.unique(labels, return_counts=True)
+		for u in range(len(unique)):
+			print('number of samples in cls {}: {}'.format(unique[u], counts[u]))
+			if return_descriptor:
+				df.loc[index[u], 'test'] = counts[u]
+
+		assert len(np.intersect1d(self.train_ids, self.test_ids)) == 0
+		assert len(np.intersect1d(self.train_ids, self.val_ids)) == 0
+		assert len(np.intersect1d(self.val_ids, self.test_ids)) == 0
+
+		if return_descriptor:
+			return df
+
+	def save_split(self, filename):
+		train_split = self.get_list(self.train_ids)
+		val_split = self.get_list(self.val_ids)
+		test_split = self.get_list(self.test_ids)
+		df_tr = pd.DataFrame({'train': train_split})
+		df_v = pd.DataFrame({'val': val_split})
+		df_t = pd.DataFrame({'test': test_split})
+		df = pd.concat([df_tr, df_v, df_t], axis=1) 
+		df.to_csv(filename, index = False)
+
+
+class Generic_MIL_Dataset(Generic_WSI_Classification_Dataset):
+	def __init__(self,
+		data_dir,
+		rna_seq_csv,
+		genes,
+		bag_dropout,
+		**kwargs):
+	
+		super(Generic_MIL_Dataset, self).__init__(**kwargs)
+		self.data_dir = data_dir
+		self.df_genes = pd.read_csv(rna_seq_csv, low_memory=False)
+		self.rna_seq_df = self.df_genes[2:]
+		self.genes = genes
+		self.bag_dropout = bag_dropout
+		self.splits_bool_df = pd.read_csv('./splits/RNAPath_main/splits_0_bool.csv')
+		
+
+
+	def __getitem__(self, idx):
+		
+		slide_id = self.slide_data['slide_id'][idx]
+		
+		label = self.df_genes.loc[self.df_genes['slides'].str.contains(slide_id[:-1]), self.df_genes.iloc[0, :].isin(self.genes)]	
+		label = torch.from_numpy(np.array(label.values.astype(np.float32))).float()
+		label = torch.log2(label + 1)
+		
+
+		if type(self.data_dir) == dict:
+			source = self.slide_data['source'][idx]
+			data_dir = self.data_dir[source]
+		else:
+			data_dir = self.data_dir
+			
+		if self.data_dir:
+			full_path = os.path.join(data_dir, 'pt_files', '{}.pt'.format(slide_id))
+			features = torch.load(full_path)
+			
+			# If bag-level dropout is applied and the slide is in the train set
+			if self.bag_dropout and slide_id in self.splits_bool_df[self.splits_bool_df['train']==True].iloc[:,0].tolist():
+				
+				# Randomly select 1 of the 4 possible patch representations
+				patch_sets = [0, 32, 64, 96]
+				n = random.randint(0, 3)
+				patch_set_i = patch_sets[n]
+				
+				if n >= 1 and os.path.exists(os.path.join(data_dir, 'pt_files', f'{slide_id}_{patch_set_i}.pt')):
+					features = torch.load(os.path.join(data_dir, 'pt_files', f'{slide_id}_{patch_set_i}.pt'))
+
+
+				# Bag-level dropout
+
+				# Calculate the number of rows to keep (random - between 70% and 100%)
+				num_rows_to_keep = np.random.randint(int(features.shape[0] * 0.7), int(features.shape[0]*1.0))
+				# Generate a random permutation of the row indices
+				row_indices = torch.randperm(features.shape[0])
+				
+				# Select the first num_rows_to_keep rows from the permuted indices
+				selected_indices = row_indices[:num_rows_to_keep]
+
+				# Keep only the selected rows
+				features = features[selected_indices, :]
+
+				return features, label
+			
+			else:
+				return features, label
+
+
+class Generic_Split(Generic_MIL_Dataset):
+	def __init__(self, slide_data, data_dir=None, num_classes=0, rna_seq_csv='./RNA-SEQ-Analysis/rnaseq_complete.csv', genes = None, bag_dropout=True ):
+		self.slide_data = slide_data
+		self.data_dir = data_dir
+		self.num_classes = num_classes
+		self.slide_cls_ids = [[] for i in range(self.num_classes)]
+		for i in range(self.num_classes):
+			self.slide_cls_ids[i] = np.where(self.slide_data['label'] == i)[0]
+
+		self.df_genes = pd.read_csv(rna_seq_csv, low_memory=False)
+		self.rna_seq_df = self.df_genes[2:]
+		self.genes = genes
+		self.bag_dropout = bag_dropout
+		self.splits_bool_df = pd.read_csv('./splits/RNAPath_main/splits_0_bool.csv')
+	
+		
+		self.genes_list = None
+
+	def __len__(self):
+		return len(self.slide_data)
+		
+
+
